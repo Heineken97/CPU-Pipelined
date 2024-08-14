c731c72d9d500f292dac6d9d1f25c8db5a2f5c4f
Joseph Jimenez <josephdjz@hotmail.com>
2024-08-14 08:03:27 -0600

    [CPU]  Updated

--- core/controlUnit.sv
+++ core/controlUnit.sv
@@ -1,5 +1,6 @@
 module controlUnit (
     input logic [3:0] opCode, // Codigo de Operacion instruccion
+	 output logic selectNextPC,// IncrementarPC
     output logic wbs,			// writeback source
     output logic mm,				// memory or mov
     output logic [2:0] ALUop, // Codigo de Operacion ALU
@@ -15,6 +16,7 @@ module controlUnit (
 );
 	// Definición de las salidas en función del opCode
 	always_comb begin
+		selectNextPC= 1'bx;
 		wbs = 1'bx;
 		mm = 1'bx;
 		ALUop = 3'bxxx;
@@ -30,10 +32,11 @@ module controlUnit (
 		case (opCode)
 			// sub
 			4'b0000: begin
+				selectNextPC= 1'b0;	//Uso del Siguiente en PC
 				wbs = 1'b1; 			//Uso de CalcData no de MemData
             mm = 1'b0;				//No pasa por memoria de datos
             ALUop = 3'b000;	
-            ri = 2'b00;				//Usa el RD2 de RegisterFile
+            ri = 2'b01;				//Usa el RD2 de RegisterFile
             wre = 1'b1;				//Usa RegisterFile
 				wm = 1'b0;				//Usa WriteRegisterData
 				am = 1'bx;				//No aplica Address or Mov
@@ -41,14 +44,15 @@ module controlUnit (
 				wme = 1'bx;				//No aplica escritura en Memoria
 				alu_mux =1'b0;			//Uso de Alu_result no de read
 				alu_mux1 =1'b1;		//Uso de Alu_result no de srcA
-				rde =1'b1;			   //Activa rd
+				rde =1'b0;			   //Activa rd de instruccion
 			end
 			// add
          4'b0001: begin
+				selectNextPC= 1'b0;	//Uso del Siguiente en PC
             wbs = 1'b1;
             mm = 1'b0;				//No pasa por memoria de datos
             ALUop = 3'b001;
-            ri = 2'b00;				//Usa el RD2 de RegisterFile
+            ri = 2'b01;				//Usa el RD2 de RegisterFile
             wre = 1'b1;				//Usa RegisterFile
 				wm = 1'b0;				//Usa WriteRegisterData
 				am = 1'bx;				//No aplica Address or Mov
@@ -56,10 +60,11 @@ module controlUnit (
 				wme = 1'bx;				//No aplica escritura en Memoria
 				alu_mux =1'b0;			//Uso de Alu_result no de read
 				alu_mux1 =1'b1;		//Uso de Alu_result no de srcA
-				rde =1'b1;			   //Activa rd
+				rde =1'b0;			   //Activa rd de instruccion
          end
 			// lsl
          4'b0010: begin
+				selectNextPC= 1'b0;	//Uso del Siguiente en PC
             wbs = 1'b1;
             mm = 1'b0;				//No pasa por memoria de datos
             ALUop = 3'b010;
@@ -71,10 +76,11 @@ module controlUnit (
 				wme = 1'bx;				//No aplica escritura en Memoria
 				alu_mux =1'b0;			//Uso de Alu_result no de read
 				alu_mux1 =1'b1;		//Uso de Alu_result no de srcA
-				rde =1'b0;			   //
+				rde =1'b0;			   //Activa rd de instruccion
          end	
 			// neg
 			4'b0011: begin
+				selectNextPC= 1'b0;	//Uso del Siguiente en PC
             wbs = 1'b1;
             mm = 1'b0;				//No pasa por memoria de datos
             ALUop = 3'b011;
@@ -86,10 +92,11 @@ module controlUnit (
 				wme = 1'bx;
 				alu_mux =1'b0;			//Uso de Alu_result no de read
 				alu_mux1 =1'b1;		//Uso de Alu_result no de srcA
-				rde =1'b0;			   //
+				rde =1'b0;			   //Activa rd de instruccion
          end	
 			// beq
 			4'b0100: begin
+				selectNextPC= 1'b0;	//Uso del Siguiente en PC
             wbs = 1'bx;
             mm = 1'b0;				//No pasa por memoria de datos
             ALUop = 3'bxxx;
@@ -101,10 +108,11 @@ module controlUnit (
 				wme = 1'bx;
 				alu_mux =1'b0;			//Uso de Alu_result no de read
 				alu_mux1 =1'b1;		//Uso de Alu_result no de srcA
-				rde =1'b0;			   //
+				rde =1'b0;			   //Activa rd de instruccion
          end  
 			// bgt
          4'b0101: begin
+				selectNextPC= 1'b0;	//Uso del Siguiente en PC
             wbs = 1'bx;
             mm = 1'b0;				//No pasa por memoria de datos
             ALUop = 3'bxxx;
@@ -116,10 +124,11 @@ module controlUnit (
 				wme = 1'bx;
 				alu_mux =1'b0;			//Uso de Alu_result no de read
 				alu_mux1 =1'b1;		//Uso de Alu_result no de srcA
-				rde =1'b0;			   //
+				rde =1'b0;			   //Activa rd de instruccion
          end
 			// blt
          4'b0110: begin
+				selectNextPC= 1'b0;	//Uso del Siguiente en PC
             wbs = 1'bx;
             mm = 1'b0;				//No pasa por memoria de datos
             ALUop = 3'bxxx;
@@ -130,10 +139,11 @@ module controlUnit (
 				ni = 1'b0;
 				wme = 1'bx;
 				alu_mux =1'bx;
-				rde =1'b0;			   //
+				rde =1'b0;			   //Activa rd de instruccion
          end
 			// b
 			4'b0111: begin
+				selectNextPC= 1'b0;	//Uso del Siguiente en PC
             wbs = 1'bx;
             mm = 1'b0;				//No pasa por memoria de datos
             ALUop = 3'bxxx;
@@ -145,10 +155,11 @@ module controlUnit (
 				wme = 1'bx;
 				alu_mux =1'b0;			//Uso de Alu_result no de read
 				alu_mux1 =1'b1;		//Uso de Alu_result no de srcA
-				rde =1'b0;			   //
+				rde =1'b0;			   //Activa rd de instruccion
          end
 			// movi
 			4'b1000: begin
+				selectNextPC= 1'b1;	//Uso del jumpAddress
             wbs = 1'b1;
             mm = 1'b0;				//No pasa por memoria de datos
             ALUop = 3'bxxx;
@@ -160,10 +171,11 @@ module controlUnit (
 				wme = 1'bx;
 				alu_mux =1'b0;			//Uso de Alu_result no de read
 				alu_mux1 =1'b1;		//Uso de Alu_result no de srcA
-				rde =1'b0;			   //
+				rde =1'b0;			   //Activa rd de instruccion
          end
 			// ldr
          4'b1001: begin
+				selectNextPC= 1'b1;	//Uso del jumpAddress
             wbs = 1'b0;
             mm = 1'b0;				//No pasa por memoria de datos
             ALUop = 3'b100;
@@ -175,10 +187,11 @@ module controlUnit (
 				wme = 1'bx;
 				alu_mux =1'b0;			//Uso de Alu_result no de read
 				alu_mux1 =1'b1;		//Uso de Alu_result no de srcA
-				rde =1'b0;			   //
+				rde =1'b0;			   //Activa rd de instruccion
          end
 			// str
          4'b1010: begin
+				selectNextPC= 1'b1;	//Uso del jumpAddress
             wbs = 1'bx;
             mm = 1'b0;				//No pasa por memoria de datos
             ALUop = 3'b100;
@@ -190,10 +203,11 @@ module controlUnit (
 				wme = 1'b1;
 				alu_mux =1'b0;			//Uso de Alu_result no de read
 				alu_mux1 =1'b1;		//Uso de Alu_result no de srcA
-				rde =1'b0;			   //
+				rde =1'b1;			   //Activa rd de writeback
          end
 			// cmp
 			4'b1011: begin
+				selectNextPC= 1'b0;	//Uso del Siguiente en PC
             wbs = 1'b1;
             mm = 1'b0;				//No pasa por memoria de datos
             ALUop = 3'b101;
@@ -205,10 +219,11 @@ module controlUnit (
 				wme = 1'bx;
 				alu_mux =1'b0;			//Uso de Alu_result no de read
 				alu_mux1 =1'b1;		//Uso de Alu_result no de srcA
-				rde =1'b0;			   //
+				rde =1'b0;			   //Activa rd de instruccion
          end
 			// movr
 			4'b1100: begin
+				selectNextPC= 1'b1;	//Uso del jumpAddress
             wbs = 1'b1;
             mm = 1'b0;				//No pasa por memoria de datos
             ALUop = 3'b011;
@@ -220,11 +235,12 @@ module controlUnit (
 				wme = 1'bx;
 				alu_mux =1'b0;			//Uso de Alu_result no de read
 				alu_mux1 =1'b1;		//Uso de Alu_result no de srcA
-				rde =1'b0;			   //
+				rde =1'b0;			   //Activa rd de writedata
          end
             
 			////////////////////// NO HA SIDO ASIGNADO //////////////////////
          4'b1101: begin
+				selectNextPC= 1'bx;	
 				wbs = 1'bx;
             mm = 1'bx;
             ALUop = 3'bxxx;
@@ -239,6 +255,7 @@ module controlUnit (
 				rde =1'bx;			   		
          end
          4'b1110: begin
+				selectNextPC= 1'bx;	
             wbs = 1'bx;
             mm = 1'bx;	
             ALUop = 3'bxxx;
@@ -253,6 +270,7 @@ module controlUnit (
 				rde =1'bx;		
          end
 			4'b1111: begin
+				selectNextPC= 1'bx;
             wbs = 1'bx;
             mm = 1'bx;				
             ALUop = 3'bxxx;
@@ -268,6 +286,7 @@ module controlUnit (
          end
             
          default: begin
+				selectNextPC= 1'bx;
 				wbs = 1'bx;
             mm = 1'bx;
             ALUop = 3'bxxx;
